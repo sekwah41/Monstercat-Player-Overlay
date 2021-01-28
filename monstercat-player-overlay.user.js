332025ec3726216cfb57c3d26cf46945cef93faf
Sekwah <business@sekwah.com>
2021-01-28 19:38:26 +0000

    Basic testing file

---
+++ monstercat-player-overlay.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Monstercat Player
+// @namespace    https://www.monstercat.com/
+// @version      0.1
+// @description  Events and code to hook into the Monstercat Player
+// @author       Sekwah
+// @match        https://www.monstercat.com/player*
+// @grant        none
+// @require      http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+/* globals $ */
+
+(function() {
+    'use strict';
+
+    $(document).ready(function() {
+        alert("HELLO");
+    });
+
+})();
